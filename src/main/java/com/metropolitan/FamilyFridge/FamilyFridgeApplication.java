3083a75816d4a4ab7e381f4d2b31b40b25c3c54d
Masa <masa.zlatkovic@hive5.studio>
2024-07-22 17:14:56 +0200

    Initial commit

---
+++ src/main/java/com/metropolitan/FamilyFridge/FamilyFridgeApplication.java
@@ -0,0 +1,13 @@
+package com.metropolitan.FamilyFridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyFridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyFridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metropolitan/FamilyFridge/FamilyFridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metropolitan.FamilyFridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyFridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
